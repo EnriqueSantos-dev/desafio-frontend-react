781d81421959f8f78d5639b903f54e91723f2549
Enrique Santos <santosenrique869@gmail.com>
2023-06-21 22:28:39 -0300

    refactor: type hook return

--- src/hooks/useGetGames.ts
+++ src/hooks/useGetGames.ts
@@ -1,9 +1,12 @@
 import { getAllGames } from "@/services/games/get-all-games";
-import { useQuery } from "@tanstack/react-query";
+import { Games } from "@/types";
+import { ApiError } from "@/types/api-error";
+import { useQuery, type UseQueryResult } from "@tanstack/react-query";
 
-export const useGetGames = () =>
-  useQuery({
+export const useGetGames = (): UseQueryResult<Games, ApiError> => {
+  return useQuery({
     queryKey: ["games"] as const,
     queryFn: getAllGames,
     retry: false,
   });
+};
