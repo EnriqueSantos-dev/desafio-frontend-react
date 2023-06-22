8036c95c63b09ac346b77f1de3583d6518eac924
Enrique Santos <santosenrique869@gmail.com>
2023-06-22 08:24:41 -0300

    feat: create custom drop-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,12 @@ module.exports = {
       },
     },
     extend: {
+      dropShadow: { errorShadow: "0px 0px 80px rgba(245, 148, 11,0.5)" },
       boxShadow: {
         innerShadowDark: "inset 0px 0px 0px 1px rgb(28,28,28)",
         innerShadowLight: "inset 0px 0px 0px 1px rgb(229, 229, 229)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
