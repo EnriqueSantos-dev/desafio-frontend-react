d86190b27220e293ca8aff981d8a4236c6d8ee2c
Enrique Santos <santosenrique869@gmail.com>
2023-06-21 23:28:25 -0300

    refactor(style): change border color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { socialLinks } from "@/constants/links";
 
 export function Header() {
   return (
-    <header className="relative border-b border-neutral-300 dark:border-neutral-800">
+    <header className="relative border-b border-neutral-200 dark:border-neutral-800">
       <div className="container flex h-16 items-center justify-between">
         <Link
           href="/"
@@ -19,7 +19,7 @@ export function Header() {
           <Link
             href={socialLinks.github}
             target="_blank"
-            className="focus-state rounded-md p-2 text-neutral-900 shadow-innerShadowLight ring-amber-500 ring-offset-neutral-500 transition-colors hover:bg-slate-100 dark:bg-neutral-950 dark:text-neutral-100 dark:shadow-innerShadowDark dark:ring-offset-neutral-900 dark:hover:bg-neutral-900"
+            className="focus-state rounded-lg p-2 text-neutral-900 shadow-innerShadowLight ring-amber-500 ring-offset-neutral-300 transition-colors hover:bg-slate-100 dark:bg-neutral-950 dark:text-neutral-100 dark:shadow-innerShadowDark dark:ring-offset-neutral-950 dark:hover:bg-neutral-900"
           >
             <GithubIcon size={20} />
           </Link>
