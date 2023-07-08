eb6a90a92da37347215b6095688477571753b441
Enrique Santos <santosenrique869@gmail.com>
2023-07-08 16:27:23 -0300

    refactor: just clear session cookie on logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,23 +1,13 @@
 import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-
-import { revokeUserTokens } from "@/lib/shared-functions/revoke-user-tokens";
+import { NextResponse } from "next/server";
 
 import { SESSION_COOKIE_NAME } from "@/constants/session-cookie";
 
-export async function POST(request: NextRequest) {
+export async function POST() {
   const cookieSession = cookies().get(SESSION_COOKIE_NAME);
 
   if (!cookieSession) return NextResponse.json(null);
 
-  try {
-    cookies().set("session", "", { maxAge: -1 });
-    await revokeUserTokens(cookieSession.value);
-    return NextResponse.json(null, { status: 204 });
-  } catch (error) {
-    console.log("POST_LOGOUT", error);
-
-    // catch by axios interceptor
-    return NextResponse.json(null, { status: 500 });
-  }
+  cookies().set(SESSION_COOKIE_NAME, "", { maxAge: -1 });
+  return NextResponse.json(null, { status: 200 });
 }
