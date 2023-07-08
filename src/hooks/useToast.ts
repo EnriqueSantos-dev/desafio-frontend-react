7ee176451e5aa0808fa9ba8a45658b68ce202862
Enrique Santos <santosenrique869@gmail.com>
2023-07-08 19:31:52 -0300

    refactor :return value memorized with useMemo

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,13 +1,20 @@
+import { useMemo } from "react";
+
 import { ReactHotToastAdapter } from "@/adapters/react-hot-toast-adapter";
 import { ToastAdapter } from "@/adapters/toast";
 
-export const toast = (toastInstance: ToastAdapter): ToastAdapter => {
-  return {
-    success: (message: string) => toastInstance.success(message),
-    error: (message: string) => toastInstance.error(message),
-  };
+export const useToastAbstraction = (
+  toastInstance: ToastAdapter
+): ToastAdapter => {
+  return useMemo(
+    () => ({
+      success: (message: string) => toastInstance.success(message),
+      error: (message: string) => toastInstance.error(message),
+    }),
+    [toastInstance]
+  );
 };
 
 export const useToast = () => {
-  return toast(new ReactHotToastAdapter());
+  return useToastAbstraction(new ReactHotToastAdapter());
 };
