0c2d7bde47cb9d0bef18f35489f2f9cb8bdb6b23
Enrique Santos <santosenrique869@gmail.com>
2023-07-09 12:38:10 -0300

    refactor(style): improve responsive on small and xl screens

--- src/app/(auth)/auth/sign-up/page.tsx
+++ src/app/(auth)/auth/sign-up/page.tsx
@@ -1,34 +1,25 @@
 import Image from "next/image";
-import { redirect } from "next/navigation";
+import { SignUpForm } from "./components/signup-form";
 
-import { getAuthSession } from "@/services/users/get-auth-session";
-import { FormLogin } from "./components/form-login";
-
-export default async function LoginPage() {
-  const session = await getAuthSession();
-
-  if (session) {
-    redirect("/");
-  }
-
+export default function LoginPage() {
   return (
     <>
       <div className="mx-auto flex w-full flex-col items-stretch justify-center rounded-2xl px-6 md:w-4/5 xl:w-full xl:px-40">
         <h1 className="text-center text-3xl/10 font-bold text-neutral-900 dark:text-neutral-100">
-          Login
+          Sign Up
         </h1>
 
-        <p className="mb-12 mt-1 text-center text-sm font-medium text-neutral-600 xl:mx-auto xl:w-3/5">
+        <p className="mb-12 mt-1 text-center text-sm font-medium text-neutral-600 xl:mx-auto xl:w-4/5">
           Discover your favorite game, rate it, share your favorites with
           friends, and have fun.
         </p>
 
-        <FormLogin />
+        <SignUpForm />
       </div>
 
       <div className="hidden rounded-lg bg-blue-200 lg:flex lg:items-center lg:justify-center">
         <Image
-          src="/login-illustration.svg"
+          src="/sign-up-illustration.svg"
           alt="illustration"
           width={500}
           height={500}
