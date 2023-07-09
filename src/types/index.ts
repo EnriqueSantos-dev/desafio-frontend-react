bfea8e0724988b51e5d146dd09ae3c1481f196a7
Enrique Santos <santosenrique869@gmail.com>
2023-07-09 12:40:06 -0300

    feat: add types for data provider from firebase

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,4 +12,27 @@ export type Game = {
   freetogame_profile_url: string;
 };
 
+export type RatingGame = {
+  game_id: number;
+  user_id: string;
+  rating: number;
+};
+
+export type FavoriteGame = {
+  game_id: number;
+  user_id: string;
+};
+
+export type GameUserDetails = {
+  gameId: number;
+  isFavorite: boolean;
+  rating: number;
+};
+
 export type Games = Game[];
+export type GameWithFavAndRating = Game & {
+  gameUserDetails: GameUserDetails;
+};
+export type GamesWithFavAndRating = (Game & {
+  gameUserDetails: GameUserDetails;
+})[];
