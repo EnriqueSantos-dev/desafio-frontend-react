732ee297e70ac69624de6a9f93874fd7030d48e2
Enrique Santos <santosenrique869@gmail.com>
2023-07-09 12:49:12 -0300

    feat: add alert-unauthorized dialog in layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,14 @@
+import { ReactNode } from "react";
+
 import { AlertExpiredSession } from "@/components/alert-expired-session";
 import { Header } from "@/components/header";
-import { ReactNode } from "react";
+import { AlertUnauthorizedUser } from "@/components/shared/alert-unathorized-user";
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <AlertExpiredSession />
+      <AlertUnauthorizedUser />
       <Header />
       {children}
     </>
