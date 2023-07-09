3ac8b6cb5b8d9c1994da6d74fe17fff758006351
Enrique Santos <santosenrique869@gmail.com>
2023-07-09 09:12:51 -0300

    refactor: put new  animations in extends object

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,35 +19,35 @@ module.exports = {
         innerShadowDark: "inset 0px 0px 0px 1px rgb(28,28,28)",
         innerShadowLight: "inset 0px 0px 0px 1px rgb(229, 229, 229)",
       },
-    },
-    animation: {
-      heartbeat: "heartbeat 1s ease-in-out infinite",
-      hearTranslateUp: "hearTranslateUp 1s ease-in-out",
-      cardAppear: "cardAppear 300ms ease-in-out var(--delay) backwards",
-    },
-    keyframes: {
-      cardAppear: {
-        from: { transform: "scale(0.9)", opacity: 0 },
-        to: { transform: "scale(1)", opacity: 1 },
+      animation: {
+        heartbeat: "heartbeat 1s ease-in-out infinite",
+        hearTranslateUp: "hearTranslateUp 1s ease-in-out",
+        cardAppear: "cardAppear 300ms ease-in-out var(--delay) backwards",
       },
-      hearTranslateUp: {
-        to: { transform: "translateY(-100px)", opacity: 0 },
-      },
-      heartbeat: {
-        "0%": {
-          transform: { scale: "1" },
+      keyframes: {
+        cardAppear: {
+          from: { transform: "scale(0.9)", opacity: 0 },
+          to: { transform: "scale(1)", opacity: 1 },
         },
-        "25%": {
-          transform: { scale: "0.8" },
+        hearTranslateUp: {
+          to: { transform: "translateY(-100px)", opacity: 0 },
         },
-        "50%": {
-          transform: "scale(1)",
-        },
-        "75%": {
-          transform: "scale(0.8)",
-        },
-        "100%": {
-          transform: "scale(1)",
+        heartbeat: {
+          "0%": {
+            transform: { scale: "1" },
+          },
+          "25%": {
+            transform: { scale: "0.8" },
+          },
+          "50%": {
+            transform: "scale(1)",
+          },
+          "75%": {
+            transform: "scale(0.8)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
         },
       },
     },
