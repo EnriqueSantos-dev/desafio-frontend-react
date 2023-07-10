24d5baff927f3120f072ee671ee9c3a339d762c6
Enrique Santos <santosenrique869@gmail.com>
2023-07-10 11:47:51 -0300

    refactor: add more erro messages

--- src/utils/get-firebase-client-messages.ts
+++ src/utils/get-firebase-client-messages.ts
@@ -2,11 +2,12 @@ const firebaseAuthClientErrosMessages = {
   "auth/user-not-found": "User not found",
   "auth/wrong-password": "Email or password is incorrect",
   "auth/email-already-exists": "User already exists",
+  "auth/email-already-in-use": "User already exists",
   "auth/too-many-requests": "Too many requests, try again later",
-};
+} as const;
 
 export function getFirebaseClientMessages(
   keyError: keyof typeof firebaseAuthClientErrosMessages
 ) {
-  return firebaseAuthClientErrosMessages[keyError];
+  return firebaseAuthClientErrosMessages[keyError] ?? "Something want wrong!";
 }
