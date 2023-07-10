0495fc5bcd70457affa71ec4e21d55ebaf6f417a
Enrique Santos <santosenrique869@gmail.com>
2023-07-09 22:14:11 -0300

    feat: create rating game service

---
+++ src/services/games/rating-game.ts
@@ -0,0 +1,18 @@
+import { apiInternal } from "@/lib/axios";
+
+export type RatingGameInput = {
+  gameId: number;
+  rating: number;
+};
+
+export type RatingGameResponse = {
+  message: string;
+};
+
+export async function ratingGame({
+  gameId,
+  rating,
+}: RatingGameInput): Promise<RatingGameResponse> {
+  const { data } = await apiInternal.put("/api/rating", { gameId, rating });
+  return data;
+}
