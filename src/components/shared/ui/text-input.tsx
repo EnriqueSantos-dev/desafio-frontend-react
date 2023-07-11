361bec7429b7793b1a0c8adc724122a64e05ddcc
Enrique Santos <santosenrique869@gmail.com>
2023-07-11 08:10:43 -0300

    style: add focus for parent element when focus is within

--- src/components/shared/ui/text-input.tsx
+++ src/components/shared/ui/text-input.tsx
@@ -9,7 +9,7 @@ const TextFieldRoot = React.forwardRef<HTMLDivElement, TextFieldRoot>(
   ({ children, className, ...props }, ref) => (
     <div
       className={cn(
-        "flex h-12 w-full items-center rounded-lg dark:bg-neutral-950 shadow-innerShadowLight dark:shadow-innerShadowDark px-2 py-1 ring-offset-neutral-50 ring-offset-2 dark:ring-offset-neutral-950 bg-neutral-50 placeholder:text-neutral-500 dark:placeholder:text-neutral-600 focus-state disabled:cursor-not-allowed disabled:opacity-50 dark:text-neutral-100 ring-neutral-200 dark:ring-neutral-800 hover:ring-2 dark:hover:ring-neutral-800 transition-all",
+        "flex h-12 w-full items-center rounded-lg dark:bg-neutral-950 shadow-innerShadowLight dark:shadow-innerShadowDark px-2 py-1 ring-offset-neutral-50 ring-offset-2 dark:ring-offset-neutral-950 bg-neutral-50 placeholder:text-neutral-500 dark:placeholder:text-neutral-600 focus-state disabled:cursor-not-allowed disabled:opacity-50 dark:text-neutral-100 ring-neutral-200 dark:ring-neutral-800 hover:ring-2 dark:hover:ring-neutral-800 transition-all focus-within:ring-2 dark:focus-within:ring-neutral-800",
         className
       )}
       tabIndex={-1}
@@ -52,7 +52,7 @@ const TextFieldAddon = React.forwardRef<HTMLButtonElement, TextFieldAddonProps>(
       <Comp
         type="button"
         className={cn(
-          "flex shrink-0 justify-center items-center dark:text-neutral-500 text-neutral-400 h-auto transition-colors dark:hover:text-neutral-400 disabled:pointer-events-none p-2 hover:text-neutral-500 bg-transparent",
+          "flex shrink-0 justify-center items-center dark:text-neutral-500 text-neutral-400 h-auto transition-colors dark:hover:text-neutral-400 disabled:pointer-events-none p-2 hover:text-neutral-500 bg-transparent focus-visible:outline-none focus-visible:text-neutral-500 dark:focus-visible:text-neutral-400",
           className
         )}
         {...props}
