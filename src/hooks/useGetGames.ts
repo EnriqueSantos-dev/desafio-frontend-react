8ccf0d83b1069da5fea9c401f8ce9a23bf9b8572
Enrique Santos <santosenrique869@gmail.com>
2023-07-13 18:57:40 -0300

    refactor: prevent ghost  fav and ratings info using isMounted strategy in component

--- src/hooks/useGetGames.ts
+++ src/hooks/useGetGames.ts
@@ -1,44 +1,64 @@
-import { useQuery, type UseQueryResult } from "@tanstack/react-query";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
 
 import { getAllGames } from "@/services/games/get-all-games";
 
-import { GamesWithFavAndRating, GameUserDetails } from "@/types";
+import { GameUserDetails, GamesWithFavAndRating } from "@/types";
+import { useEffect, useState } from "react";
 import { ApiError } from "@/types/api-error";
 
-export const useGetGames = (
-  favGamesAndRatings: GameUserDetails[]
-): UseQueryResult<GamesWithFavAndRating, ApiError> => {
-  return useQuery({
+type ResultQuery = UseQueryResult<GamesWithFavAndRating, ApiError>;
+
+export const useGetGames = (favGamesAndRatings: GameUserDetails[]) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const query = useQuery({
     queryKey: ["games"] as const,
-    queryFn: async () => {
-      const games = await getAllGames();
-
-      return games.map((game) => {
-        const gameFound = favGamesAndRatings.find(
-          (favGameAndRating) => favGameAndRating.gameId === game.id
-        );
-
-        if (gameFound) {
-          return {
-            ...game,
-            gameUserDetails: {
-              isFavorite: gameFound.isFavorite,
-              rating: gameFound.rating,
-            },
-          };
-        }
-
-        return {
-          ...game,
-          gameUserDetails: {
-            isFavorite: false,
-            rating: 0,
-          },
-        };
-      });
-    },
+    queryFn: getAllGames,
     retry: false,
     refetchOnWindowFocus: false,
     staleTime: Infinity,
   });
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return { ...query } as ResultQuery;
+  }
+
+  return {
+    ...query,
+    data: query.data?.map((game) => {
+      const favGame = favGamesAndRatings.find(
+        (favGame) => favGame.gameId === game.id
+      );
+
+      if (favGame) {
+        return {
+          ...game,
+          gameUserDetails: {
+            gameId: game.id,
+            isFavorite:
+              typeof game.gameUserDetails.isFavorite !== "undefined"
+                ? game.gameUserDetails.isFavorite
+                : favGame.isFavorite,
+            rating:
+              typeof game.gameUserDetails.rating !== "undefined"
+                ? game.gameUserDetails.rating
+                : favGame.rating,
+          },
+        };
+      }
+
+      return {
+        ...game,
+        gameUserDetails: {
+          gameId: game.id,
+          isFavorite: false,
+          rating: 0,
+        },
+      };
+    }),
+  } as ResultQuery;
 };
