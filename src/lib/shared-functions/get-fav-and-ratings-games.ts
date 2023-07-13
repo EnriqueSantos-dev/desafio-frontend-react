f5954b7111d6d76f266427b7ef212c43053a8030
Enrique Santos <santosenrique869@gmail.com>
2023-07-13 19:06:08 -0300

    refactor: simplify code to get rantings and favorites games

--- src/lib/shared-functions/get-fav-and-ratings-games.ts
+++ src/lib/shared-functions/get-fav-and-ratings-games.ts
@@ -4,55 +4,31 @@ import { getAuthSession } from "@/services/users/get-auth-session";
 
 import { GameUserDetails } from "@/types";
 
-import { getFavoriteGames } from "@/lib/shared-functions/get-fav-games";
-import { getGamesRatings } from "@/lib/shared-functions/get-games-ratings";
-
 export async function getFavAndRatingsGames() {
   const session = await getAuthSession();
-  let mappedGames: GameUserDetails[] = [];
 
-  if (session) {
-    const connection = getDatabaseAdmin();
-    const favoritesGames = await getFavoriteGames(connection, session.uid);
-    const ratingsUserGames = await getGamesRatings(connection, session.uid);
-
-    const mergedList = favoritesGames.reduce(
-      (result, item, i) => {
-        let existingItem = result.find(
-          (mergedItem) => mergedItem.game_id === item.game_id
-        );
-
-        if (existingItem) {
-          // Item já existe, mesclar informações
-          existingItem = Object.assign(existingItem, {
-            ...item,
-            isFavorite: true,
-          });
-        } else {
-          // Item não existe, adicionar à lista
-          result.push({
-            ...item,
-            rating: result[i]?.rating ?? 0,
-            isFavorite: true,
-          });
-        }
-
-        return result;
-      },
-      [
-        ...ratingsUserGames.map((ratingGame) => ({
-          ...ratingGame,
-          isFavorite: false,
-        })),
-      ]
-    );
-
-    mappedGames = mergedList.map((mergedGame) => ({
-      gameId: mergedGame.game_id,
-      isFavorite: mergedGame.isFavorite,
-      rating: mergedGame.rating,
-    }));
+  if (!session) {
+    return [];
   }
 
-  return mappedGames;
+  const connection = getDatabaseAdmin();
+  let rantingsAndFavGames: GameUserDetails[] = [];
+
+  const snapshot = await connection
+    .ref(`games/users/${session.uid}/ratings_and_favorites`)
+    .get();
+
+  if (snapshot.exists()) {
+    snapshot.forEach((childSnapshot) => {
+      const data = childSnapshot.val();
+
+      rantingsAndFavGames.push({
+        gameId: parseInt(childSnapshot!.key!.replace("id_", "")),
+        isFavorite: data.isFavorite,
+        rating: data.rating,
+      });
+    });
+  }
+
+  return rantingsAndFavGames;
 }
--- src/services/games/get-all-games.ts
+++ src/services/games/get-all-games.ts
@@ -5,14 +5,13 @@ const TIMEOUT = 5000;
 
 export async function getAllGames(): Promise<GamesWithFavAndRating> {
   const { data } = await api.get<Games>("/data", { timeout: TIMEOUT });
-  return (
-    data?.map((game) => ({
-      ...game,
-      gameUserDetails: {
-        gameId: game.id,
-        isFavorite: undefined,
-        rating: undefined,
-      },
-    })) ?? []
-  );
+
+  return data?.map((game) => ({
+    ...game,
+    gameUserDetails: {
+      gameId: game.id,
+      isFavorite: undefined,
+      rating: undefined,
+    },
+  }));
 }
