d8e00b1636b075db6fa9f6db82a810c5dbb844e3
Enrique Santos <santosenrique869@gmail.com>
2023-07-14 19:59:38 -0300

    feat: util function to get base64 file

---
+++ src/utils/get-base64.ts
@@ -0,0 +1,9 @@
+export const getBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = () =>
+      reject(new Error("Error while reading file", { cause: "getBase64" }));
+  });
+};
