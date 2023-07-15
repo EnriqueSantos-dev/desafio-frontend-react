fb79ad27984cb82b248630b22b59d9fa371bee93
Enrique Santos <santosenrique869@gmail.com>
2023-07-15 13:08:36 -0300

    feat: create hook useGetCommunityReviews

---
+++ src/hooks/useGetCommunityReviews.ts
@@ -0,0 +1,36 @@
+import { getAllGames } from "@/services/games/get-all-games";
+
+import { CommunityReview, CommunityReviewsAndGames } from "@/types";
+
+import { ApiError } from "@/types/api-error";
+
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+
+export const useGetCommunityReviewsAndGames = (reviews: CommunityReview[]) => {
+  const query = useQuery({
+    queryKey: ["community-reviews"],
+    queryFn: getAllGames,
+  });
+
+  return {
+    ...query,
+    data: query.data
+      ?.filter((game) => {
+        return reviews.find((review) => review.gameId === game.id);
+      })
+      .map((game, i) => {
+        const { gameUserDetails, ...rest } = game;
+
+        const gameReviewIndex = reviews.findIndex(
+          (review) => review.gameId === game.id
+        );
+
+        if (gameReviewIndex > -1) {
+          return {
+            ...rest,
+            ...reviews[i],
+          };
+        }
+      }),
+  } as UseQueryResult<CommunityReviewsAndGames, ApiError>;
+};
