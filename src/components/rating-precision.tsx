a2e979012d68f00d6d08936c8dce7b0dcaf11bf6
Enrique Santos <santosenrique869@gmail.com>
2023-07-15 15:04:58 -0300

    style: add space-x in parent element

--- src/components/rating-precision.tsx
+++ src/components/rating-precision.tsx
@@ -11,13 +11,13 @@ export function RatingPrecision({
   reviewsCount,
 }: RatingPrecisionProps) {
   return (
-    <div className="flex items-center text-neutral-900 dark:text-neutral-100">
+    <div className="flex items-center space-x-2 text-neutral-900 dark:text-neutral-100">
       <Star size={24} className="mb-px fill-yellow-500 stroke-yellow-500" />
-      <p className="ml-2 text-sm font-bold text-neutral-900 dark:text-neutral-100">
+      <p className="text-sm font-bold text-neutral-900 dark:text-neutral-100">
         {average.toFixed(2)}
       </p>
 
-      <span className="mx-1.5 h-1 w-1 rounded-full bg-neutral-700 dark:bg-neutral-800"></span>
+      <span className="mx-1.5 h-1 w-1 rounded-full bg-neutral-700 dark:bg-neutral-600"></span>
       <span className="text-sm font-medium">{reviewsCount} reviews</span>
     </div>
   );
