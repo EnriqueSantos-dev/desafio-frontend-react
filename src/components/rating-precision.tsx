4b18f101aa9cda987ef5248861c31f651e3a8c67
Enrique Santos <santosenrique869@gmail.com>
2023-07-15 14:35:26 -0300

    refactor: remove unused property

--- src/components/rating-precision.tsx
+++ src/components/rating-precision.tsx
@@ -8,7 +8,6 @@ type RatingPrecisionProps = {
 
 export function RatingPrecision({
   average,
-  totalReviews,
   reviewsCount,
 }: RatingPrecisionProps) {
   return (
@@ -19,12 +18,7 @@ export function RatingPrecision({
       </p>
 
       <span className="mx-1.5 h-1 w-1 rounded-full bg-neutral-700 dark:bg-neutral-800"></span>
-      <span className="text-sm font-medium">
-        {new Intl.NumberFormat("en-US", {
-          compactDisplay: "short",
-        }).format(totalReviews)}{" "}
-        out of {reviewsCount}
-      </span>
+      <span className="text-sm font-medium">{reviewsCount} reviews</span>
     </div>
   );
 }
