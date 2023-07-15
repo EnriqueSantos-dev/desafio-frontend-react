87034a76c3bbc77744aa75b0dceb433b9df185f6
Enrique Santos <santosenrique869@gmail.com>
2023-07-15 15:32:22 -0300

    refactor: handle error on call request inside a hook

--- src/hooks/useGetCommunityReviews.ts
+++ src/hooks/useGetCommunityReviews.ts
@@ -1,3 +1,4 @@
+import { useToast } from "@/hooks/useToast";
 import { getAllGames } from "@/services/games/get-all-games";
 
 import { CommunityReview, CommunityReviewsAndGames } from "@/types";
@@ -5,14 +6,21 @@ import { CommunityReview, CommunityReviewsAndGames } from "@/types";
 import { ApiError } from "@/types/api-error";
 
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export const useGetCommunityReviewsAndGames = (reviews: CommunityReview[]) => {
-  const query = useQuery({
+  const { error } = useToast();
+  const query = useQuery<Awaited<ReturnType<typeof getAllGames>>, ApiError>({
     queryKey: ["community-reviews"],
     queryFn: getAllGames,
     retry: false,
     refetchOnWindowFocus: false,
   });
+
+  useEffect(() => {
+    if (query.error) error(query.error.message);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query.error]);
 
   return {
     ...query,
